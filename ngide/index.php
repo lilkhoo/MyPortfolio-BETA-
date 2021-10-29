382b46f1cf8aa69324fb6e08052ac352771b9182
ekoongt123 <setyonobowo24@gmail.com>
2021-10-29 08:37:04 +0700

    Menambahkan Folder Portfolio

---
+++ ngide/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>testing</title>
+</head>
+<body>
+	<h3>UWEM <?php echo "santuy";  ?></h3>
+</body>
+</html>
+
+<?php
+
+$kopi=5000;
+$uang=10000;
+
+$total;
+
+$total = $kopi - $uang;
+echo "total" . $total;
+
+ ?>
